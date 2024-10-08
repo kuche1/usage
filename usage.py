531e12024f29e34376ae4bcffb6459866096f978
kuche1 <znamsiparolata4esno@abv.bg>
2024-10-08 17:07:10 +0300

    update

--- usage.py
+++ usage.py
@@ -5,9 +5,14 @@ import time
 import argparse
 from typing import Optional
 import os
+import drawille
 
 IGNORE_USER_CPU_THRESH = 0.0
-HISTORY_MAXLEN = 100
+HISTORY_MAXLEN = 200
+DETERMINE_COLOR_BASED_ON_HIGHEST_AVG = True # otherwise: based on highest
+
+BRILE_WIDTH_MUL = 2 # brile characters are 0.5 the width of a normal character
+BRILE_HEIGHT_MUL = 4 # brile characters are 0.25 the height of a normal character
 
 COL_RESET  = '\u001b[0m'
 COL_RED    = '\u001b[31m'
@@ -17,148 +22,208 @@ COL_BLUE   = '\u001b[34m'
 
 def main(only_show_user:Optional[str], iter_sleep:float):
 
-    if only_show_user != None:
-        cpu_history = []
+    cpu_history = []
 
     while True:
 
         # quickly remember all processes
-
-        user_procs = {}
-
-        for proc in psutil.process_iter():
-
-            try:
-                user = proc.username()
-                name = proc.name()
-                cpu_usage = proc.cpu_percent()
-                mem = proc.memory_info()
-            except psutil.NoSuchProcess:
-                continue
-            
-            if only_show_user != None:
-                if user != only_show_user:
-                    continue
-
-            mem_non_swap_bytes = mem.rss
-            mem_all_bytes = mem.vms
-
-            if user not in user_procs:
-                user_procs[user] = []
-
-            user_procs[user].append((name, cpu_usage, mem_non_swap_bytes))
+        user_procs = get_processes(only_show_user)
 
         # calc user usage
-
-        user_usages = []
-
-        for user, procs in user_procs.items():
-            
-            cpu = 0
-            mem = 0
-            for _p_name, p_cpu, p_mem in procs:
-                cpu += p_cpu
-                mem += p_mem
-
-            user_usages.append((user, cpu, mem))
+        user_usages = calc_user_usages(user_procs)
 
         # save history
+        save_history(only_show_user, cpu_history, user_usages)
 
+        # prepare buffered output
+        output = ''
+
+        # draw separator
+        output += '\n'
+
+        # draw history
+        output += draw_history(only_show_user, cpu_history)
+
+        # print usage
+        output += print_usage(only_show_user, user_usages)
+
+        # show buffered output
+        print(output, end='')
+
+        # sleep
+        time.sleep(iter_sleep)
+
+def get_processes(only_show_user):
+    user_procs = {}
+
+    for proc in psutil.process_iter():
+
+        try:
+            user = proc.username()
+            name = proc.name()
+            cpu_usage = proc.cpu_percent()
+            mem = proc.memory_info()
+        except psutil.NoSuchProcess:
+            continue
+
+        if only_show_user != None:
+            if user != only_show_user:
+                continue
+
+        mem_non_swap_bytes = mem.rss
+        mem_all_bytes = mem.vms
+
+        if user not in user_procs:
+            user_procs[user] = []
+
+        user_procs[user].append((name, cpu_usage, mem_non_swap_bytes))
+
+    return user_procs
+
+def calc_user_usages(user_procs):
+    user_usages = []
+
+    for user, procs in user_procs.items():
+
+        cpu = 0
+        mem = 0
+        for _p_name, p_cpu, p_mem in procs:
+            cpu += p_cpu
+            mem += p_mem
+
+        user_usages.append((user, cpu, mem))
+
+    return user_usages
+
+def save_history(only_show_user, cpu_history, user_usages):
+    if only_show_user:
         for user, cpu, _mem in user_usages:
             if user == only_show_user:
                 cpu_history.insert(0, cpu)
                 while len(cpu_history) > HISTORY_MAXLEN:
                     del cpu_history[-1]
+                break
 
-        # draw separator
+def draw_history(only_show_user, cpu_history):
+    output = ''
 
-        print()
-        print()
+    if only_show_user:
 
         terminal_size = os.get_terminal_size()
 
-        # draw history
+        free_space_y = terminal_size.lines - 1
+        free_space_y *= BRILE_HEIGHT_MUL
+        history = cpu_history[:free_space_y]
 
-        if only_show_user:
+        highest_value = max(history)
+        if highest_value == 0:
+            highest_value = 0.1
 
-            free_space_y = terminal_size.lines - 2
-            history = cpu_history[:free_space_y]
+        # ...
 
-            highest = max(history)
-            if highest == 0:
-                highest = 1
+        cur_line_values = []
+        all_line_values = []
 
-            value_prev = 0
-            for value in reversed(history):
+        for idx, graph_elem_value in enumerate(history):
 
-                # if value >= highest * 2/3:
-                #     col = COL_RED
-                # elif value >= highest * 1/3:
-                #     col = COL_YELLOW
-                # else:
-                #     col = COL_GREEN
+            cur_line_values.insert(0, graph_elem_value)
 
-                highness = value / highest
+            if idx % BRILE_HEIGHT_MUL == BRILE_HEIGHT_MUL - 1:
+                all_line_values.insert(0, cur_line_values)
+                cur_line_values = []
 
-                col = '\u001b[38;2;'
-                col += str(int(220 * highness)) + ';'
-                col += str(int(220 * (1.0 - highness))) + ';'
-                col += f'30'
-                col += 'm'
+        if len(cur_line_values):
+            while len(cur_line_values) < BRILE_HEIGHT_MUL:
+                cur_line_values.insert(0, 0)
+            all_line_values.insert(0, cur_line_values)
 
-                free_space_x = terminal_size.columns - 6 - 3 - 2
-                length = (value / highest) * free_space_x
-                length = int(length)
-                print(f'{value:6.2f}[%]', end='')
+        # ...
 
-                print(col, end='')
+        highest_avg_value = 0.1
 
-                print('|', end='')
+        for line_values in all_line_values:
+            avg = sum(line_values) / len(line_values)
+            if avg > highest_avg_value:
+                highest_avg_value = avg
 
-                print('#' * length, end='')
+        # ...
 
-                if value > value_prev:
-                    print('\\', end='')
-                elif value < value_prev:
-                    print('/', end='')
-                else:
-                    print('|', end='')
+        for line_values in all_line_values:
 
-                print(COL_RESET)
+            avg_value = sum(line_values) / len(line_values)
+            output += f'{avg_value:6.2f}[%]'
 
-                value_prev = value
+            canvas = drawille.Canvas()
 
-        # print usage
+            for y, value in enumerate(line_values):
 
-        longest_username = 0
+                free_space_x = terminal_size.columns - 6 - 3
+                free_space_x *= BRILE_WIDTH_MUL
+                free_space_x *= (value / highest_value)
+                free_space_x = int(free_space_x)
 
-        for user, cpu, _ in user_usages:
+                for x in range(free_space_x):
+                    canvas.set(x, y)
 
-            length = len(user)
-            if length > longest_username:
-                longest_username = length
+            if DETERMINE_COLOR_BASED_ON_HIGHEST_AVG:
+                redness = avg_value / highest_avg_value
+            else:
+                redness = avg_value / highest_value
+            col = '\u001b[38;2;'
+            col += str(int(220 * redness)) + ';'
+            col += str(int(220 * (1.0 - redness))) + ';'
+            col += f'30'
+            col += 'm'
 
-        user_usages.sort(key=lambda i: i[1])
+            output += col
+            output += canvas.frame()
+            output += COL_RESET
 
-        for idx, (user, cpu, mem) in enumerate(user_usages):
+            output += '\n'
 
-            if cpu <= IGNORE_USER_CPU_THRESH:
-                continue
+    return output
 
-            fill_char = '~' if idx % 2 == 0 else '-'
+def print_usage(only_show_user, user_usages):
+    output = ''
 
-            user += ' '
-            user = user.ljust(longest_username + 1, fill_char)
+    longest_username = 0
 
-            cpu = f'{cpu:.2f}'
-            cpu = ' ' + cpu
-            cpu = cpu.rjust(7, fill_char)
+    for user, cpu, _ in user_usages:
 
-            print(f'{user}{cpu}[%] {mem/1024/1024/1024:5.2f}[GB]')
+        length = len(user)
+        if length > longest_username:
+            longest_username = length
 
-        # sleep
+    user_usages.sort(key=lambda i: i[1])
 
-        time.sleep(iter_sleep)
+    user_usages_output = ''
+
+    for idx, (user, cpu, mem) in enumerate(user_usages):
+
+        if cpu <= IGNORE_USER_CPU_THRESH:
+            continue
+
+        fill_char = '~' if idx % 2 == 0 else '-'
+
+        user += ' '
+        user = user.ljust(longest_username + 1, fill_char)
+
+        cpu = f'{cpu:.2f}'
+        cpu = ' ' + cpu
+        cpu = cpu.rjust(7, fill_char)
+
+        user_usages_output += f'{user}{cpu}[%] {mem/1024/1024/1024:5.2f}[GB]'
+        user_usages_output += '\n'
+
+    if only_show_user:
+        if user_usages_output.endswith('\n'):
+            user_usages_output = user_usages_output[:-1]
+    else:
+        user_usages_output += '\n'
+
+    output += user_usages_output
+
+    return output
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('shows usage based on user')
